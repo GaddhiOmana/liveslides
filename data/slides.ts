e98a8e3d7b6357336569ad9a18bcd9f4e0c5efd1
Gaddhi <gadi.omana@gmail.com>
2025-11-08 08:03:35 -0600

    update slides

--- data/slides.ts
+++ data/slides.ts
@@ -1,4 +1,40 @@
 export const slides = [
   { src: "https://i.ibb.co/PshdMB4r/01-c.jpg", alt: "Viaje al futuro" },
   { src: "https://i.ibb.co/5XqZPst8/001.jpg", alt: "La Batalla Final" },
+  {
+    src: "https://i.ibb.co/JwpXJDfK/003-c.jpg",
+    alt: "Resurreción de los impíos",
+  },
+  {
+    src: "https://i.ibb.co/3YfkQ6rv/04-c.png",
+    alt: "Satanás engaña",
+  },
+  {
+    src: "https://i.ibb.co/pjxDDPtw/05-c.png",
+    alt: "Preparativos del ejército de los perdidos",
+  },
+  {
+    src: "https://i.ibb.co/N07TsLv/06-c.png",
+    alt: "Asedio a la Ciudad Santa",
+  },
+  {
+    src: "https://i.ibb.co/dwPYJkg0/07-c.png",
+    alt: "Creación de un nuevo y triunfo eterno",
+  },
+  {
+    src: "https://i.ibb.co/fPXcpYV/08-c.png",
+    alt: "Creación de un nuevo y triunfo eterno",
+  },
+  {
+    src: "https://i.ibb.co/fVbvV2rg/09-c.png",
+    alt: "Creación de un nuevo y triunfo eterno",
+  },
+  {
+    src: "https://i.ibb.co/4RTXjPfx/10-c.png",
+    alt: "Los mártires de todas las edades",
+  },
+  {
+    src: "https://i.ibb.co/pr9Dd82W/11-c.png",
+    alt: "Los que cantan el cántico de Moisés",
+  },
 ];
